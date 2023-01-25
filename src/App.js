de0d79a3fed096d534dc18840806bf48ab431220
lazarostar <nascimento.dev26@gmail.com>
2023-01-25 05:13:37 +0100

    feat: game frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { initializeGame } from "./game";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    initializeGame({
+      onStarted: () => alert("Started"),
+      onFinished: (score) => alert(`Finished with score ${score}`),
+    });
+  }, []);
+  return <div id="game"></div>;
 }
 
 export default App;
