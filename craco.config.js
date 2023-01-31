1097fe82752a1f45bbf3e306c68c75f0f55b3c79
lazarostar <nascimento.dev26@gmail.com>
2023-01-31 14:49:47 +0100

    feat: dockerize

--- craco.config.js
+++ craco.config.js
@@ -20,6 +20,20 @@ module.exports = {
         new webpack.ProvidePlugin({
           Buffer: ["buffer", "Buffer"],
         }),
+        new webpack.DefinePlugin({
+          "process.env.REACT_APP_SERVER_HOST": JSON.stringify(
+            process.env.REACT_APP_SERVER_HOST
+          ),
+          "process.env.REACT_APP_API_URL": JSON.stringify(
+            process.env.REACT_APP_API_URL
+          ),
+          "process.env.REACT_APP_PRIZE_WALLET": JSON.stringify(
+            process.env.REACT_APP_PRIZE_WALLET
+          ),
+          "process.env.REACT_APP_PURCHASE_AMOUNT": JSON.stringify(
+            process.env.REACT_APP_PURCHASE_AMOUNT
+          ),
+        }),
       ],
     },
   },
