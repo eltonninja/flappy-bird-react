92ccbb89459368965d787d739e396371e8ffe3a5
lazarostar <nascimento.dev26@gmail.com>
2023-02-02 08:10:57 +0100

    refactor: craco for docker

--- craco.config.js
+++ craco.config.js
@@ -21,20 +21,20 @@ module.exports = {
           process: "process/browser",
           Buffer: ["buffer", "Buffer"],
         }),
-        // new webpack.DefinePlugin({
-        //   "process.env.REACT_APP_SERVER_HOST": JSON.stringify(
-        //     process.env.REACT_APP_SERVER_HOST
-        //   ),
-        //   "process.env.REACT_APP_API_URL": JSON.stringify(
-        //     process.env.REACT_APP_API_URL
-        //   ),
-        //   "process.env.REACT_APP_PRIZE_WALLET": JSON.stringify(
-        //     process.env.REACT_APP_PRIZE_WALLET
-        //   ),
-        //   "process.env.REACT_APP_PURCHASE_AMOUNT": JSON.stringify(
-        //     process.env.REACT_APP_PURCHASE_AMOUNT
-        //   ),
-        // }),
+        new webpack.DefinePlugin({
+          "process.env.REACT_APP_SERVER_HOST": JSON.stringify(
+            process.env.REACT_APP_SERVER_HOST
+          ),
+          "process.env.REACT_APP_API_URL": JSON.stringify(
+            process.env.REACT_APP_API_URL
+          ),
+          "process.env.REACT_APP_PRIZE_WALLET": JSON.stringify(
+            process.env.REACT_APP_PRIZE_WALLET
+          ),
+          "process.env.REACT_APP_PURCHASE_AMOUNT": JSON.stringify(
+            process.env.REACT_APP_PURCHASE_AMOUNT
+          ),
+        }),
       ],
     },
     configure: {
